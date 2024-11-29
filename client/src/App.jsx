a088ecfe7e1c1a0f74bd487669e417d35961b6ee
Guna <gvengalasetti@gmail.com>
2024-11-29 14:27:58 -0800

    update with correct package.json npm installs etc.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import UpdatePage from "./routes/UpdatePage";
 import RestaurantDetailPage from "./routes/RestaurantDetailPage";
 import { RestaurantsContextProvider } from "./context/RestaurantsContext";
+
 const App = () => {
   return (
     <RestaurantsContextProvider>
       <div className="container">
         <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route
-              exact
-              path="/restaurants/:id/update"
-              component={UpdatePage}
-            />
-            <Route
-              exact
-              path="/restaurants/:id"
-              component={RestaurantDetailPage}
-            />
-          </Switch>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+            <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+          </Routes>
         </Router>
       </div>
     </RestaurantsContextProvider>
