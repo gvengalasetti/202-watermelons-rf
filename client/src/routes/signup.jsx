d8e03b4f3f24953fe0b6084500814f1ce9a24563
Siddhant412 <siddhantraje2000@gmail.com>
2024-12-10 17:31:25 -0800

    nginx urls updated

--- client/src/routes/signup.jsx
+++ client/src/routes/signup.jsx
@@ -34,7 +34,7 @@ const SignupPage = () => {
 
     // Call signup API
     try {
-      const response = await fetch("http://35.171.154.179:5001/api/signup", {
+      const response = await fetch("/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
